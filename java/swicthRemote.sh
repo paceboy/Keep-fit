df529307f54f3e2b4f344d24f3f1df0b3224cbee
liushaogeng <liushaogeng@baidu.com>
2021-03-22 20:39:28 +0800

    java模块增加切换git库的脚本
    Change-Id: I6147cb0668d57a6504da42cac3fceb0870e4e6c5

---
+++ java/swicthRemote.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+remote=https://github.com/paceboy/Keep-fit.git
+#remote=ssh://liushaogeng@icode.baidu.com:8235/baidu/goodcoder/liushaogeng 
+
+#git clone --mirror $remote
+git remote set-url origin $remote
+
