7f92a2983477d2e967d6b840542c07e307be0f3a
liushaogeng <liushaogeng@baidu.com>
2023-04-09 11:10:12 +0800

    更新readme

--- swicthRemote.sh
+++ swicthRemote.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-remote=https://github.com/paceboy/Keep-fit.git
+#remote=https://github.com/paceboy/Keep-fit.git
+remote=git@github.com:paceboy/Keep-fit.git
 #remote=ssh://liushaogeng@icode.baidu.com:8235/baidu/goodcoder/liushaogeng
 
 #git clone --mirror $remote
