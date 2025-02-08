325a91c59c738b737825e2cad459b91552179a30
liushaogeng <liushaogeng@baidu.com>
2025-02-08 22:35:10 +0800

    增加按照一定序列来输出二叉树

--- java/src/main/java/com/accumulation/answer/BinaryTree.java
+++ java/src/main/java/com/accumulation/answer/BinaryTree.java
@@ -51,4 +51,65 @@ public class BinaryTree {
         }
         return levelOrder;
     }
+
+    /*
+     给定这样一个二叉树结构，对这样一个root节点，从左子节点开始蛇形层次遍历输出整棵树的节点值
+    比如：
+            1
+         |-----|
+         2     3
+       |---|   ---|
+       4   5      6
+    |---   ---|
+    7         8
+    输出12365478
+
+    比如：
+            a
+         |-----|
+         b     c
+       |---|
+       d   e
+           ---|
+              h
+    输出abcedh
+
+     * Param: root 传入的二叉树根节点
+     * Return：返回
+     * */
+    public List<List<Integer>> levelOrderByYourself(BinaryTreeNode root) {
+        List<List<Integer>> levelOrder = new LinkedList();
+        if (root == null) {
+            return levelOrder;
+        }
+
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            List<Integer> singleLevel = new ArrayList();
+            int size = queue.size();
+            for(int i=0;i<size;i++) {
+                BinaryTreeNode binaryTreeNode = queue.poll();
+                if (level % 2 == 0) {
+                    singleLevel.add(0, binaryTreeNode.val);
+                } else {
+                    singleLevel.add(binaryTreeNode.val);
+                }
+
+                BinaryTreeNode left = binaryTreeNode.left;
+                BinaryTreeNode right = binaryTreeNode.right;
+                if (left != null) {
+                    queue.offer(left);
+                }
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+            levelOrder.add(singleLevel);
+            level++;
+        }
+        return levelOrder;
+    }
+
 }
--- java/src/main/java/com/question/Main.java
+++ java/src/main/java/com/question/Main.java
@@ -21,21 +21,21 @@ public class Main {
 //        System.out.println (minumPath.minPathSum(grid1));
 
         // 二叉树的按叶子结点往根节点的排序遍历
-        BinaryTree binaryTree = new BinaryTree();
-        BinaryTreeNode root ;
-//      Example root = [3,9,20,null,null,15,7]
-        BinaryTreeNode left = new BinaryTreeNode(9, null, null);
-        BinaryTreeNode right = new BinaryTreeNode(20 , new BinaryTreeNode(15), new BinaryTreeNode(7));
-        BinaryTreeNode node = new BinaryTreeNode(3, left, right);
-        root =  node;
-        List<List<Integer>> listOfLists = binaryTree.levelOrderBottomByYourself(root);
-        for(List<Integer> innerList : listOfLists) {
-            for(Integer number : innerList) {
-                System.out.print(number);
-                System.out.print(',');
-            }
-            System.out.println();
-        }
+//        BinaryTree binaryTree = new BinaryTree();
+//        BinaryTreeNode root ;
+////      Example root = [3,9,20,null,null,15,7]
+//        BinaryTreeNode left = new BinaryTreeNode(9, null, null);
+//        BinaryTreeNode right = new BinaryTreeNode(20 , new BinaryTreeNode(15), new BinaryTreeNode(7));
+//        BinaryTreeNode node = new BinaryTreeNode(3, left, right);
+//        root =  node;
+//        List<List<Integer>> listOfLists = binaryTree.levelOrderBottomByYourself(root);
+//        for(List<Integer> innerList : listOfLists) {
+//            for(Integer number : innerList) {
+//                System.out.print(number);
+//                System.out.print(',');
+//            }
+//            System.out.println();
+//        }
 
 //        ClimbStairs climbStairs = new ClimbStairs();
 //        int ret = climbStairs.solution(7);
@@ -64,5 +64,23 @@ public class Main {
 //        pn = "1234";
 //        ReformatNumber.formatNumber(pn);
 //        System.out.printf("formatNumber : pn = %s, formatNumber = %s", pn, ReformatNumber.formatNumber(pn));
+        BinaryTree binaryTree = new BinaryTree();
+        BinaryTreeNode root ;
+        BinaryTreeNode left1 = new BinaryTreeNode(1, new BinaryTreeNode(4), new BinaryTreeNode(5));
+        BinaryTreeNode right1 = new BinaryTreeNode(2, new BinaryTreeNode(3), new BinaryTreeNode(2));
+        BinaryTreeNode left = new BinaryTreeNode(9, left1, right1);
+        BinaryTreeNode left2 = new BinaryTreeNode(10, new BinaryTreeNode(11), new BinaryTreeNode(12));
+        BinaryTreeNode right2 = new BinaryTreeNode(13, new BinaryTreeNode(14), new BinaryTreeNode(15));
+        BinaryTreeNode right = new BinaryTreeNode(20 , left2, right2);
+        BinaryTreeNode node = new BinaryTreeNode(3, left, right);
+        root =  node;
+        List<List<Integer>> listOfLists = binaryTree.levelOrderBottomByYourself(root);
+        for(List<Integer> innerList : listOfLists) {
+            for(Integer number : innerList) {
+                System.out.print(number);
+                System.out.print(',');
+            }
+            System.out.println();
+        }
     }
 }
